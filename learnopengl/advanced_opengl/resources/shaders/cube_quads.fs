581a411ee03a9034cf7daf62353100fdabae2a9e
edselmalasig <edsel.malasig@me.com>
2021-02-25 18:52:57 -0800

    I hope this works.

---
+++ learnopengl/advanced_opengl/resources/shaders/cube_quads.fs
@@ -0,0 +1,14 @@
+#version 330 core
+out vec4 FragColor;
+
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+uniform int mode;
+void main()
+{
+     //if(mode == 0 || mode == 1){
+          //FragColor = vec4(objectColor, 1.0);
+     //}else{
+         FragColor = vec4(objectColor * lightColor, 1.0);
+     //}
+}
